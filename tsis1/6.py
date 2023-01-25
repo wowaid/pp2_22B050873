10d0fa0d6dd538e56891f0256fc3dad66f4182db
wowaid <123165906+wowaid@users.noreply.github.com>
2023-01-25 21:42:47 +0600

    Create 6.py

---
+++ tsis1/6.py
@@ -0,0 +1,19 @@
+x = 5
+a= "asd"
+b = 20.5
+c = 1j
+d = ["apple", "banana", "cherry"]
+e = ("apple", "banana", "cherry")
+e = range(6)
+f = {"name" : "John", "age" : 36}
+g = {"apple", "banana", "cherry"}
+h = True
+print(type(x))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
