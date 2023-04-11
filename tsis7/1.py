921f30646e4c58bfb85453c76be11d1a02885777
Aidana <ayesjan00@gmail.com>
2023-04-11 14:31:04 +0600

    update

--- tsis7/1.py
+++ tsis7/1.py
@@ -1,20 +1,14 @@
 import pygame as pg
 from datetime import datetime
-# from datetime import datetime
-# import datetime
-
 
 pg.init()
 
 WIDTH = 800
 HEIGHT = 600
 FPS = 45
-BLACK = (0,0,0)
 
 pg.display.set_caption('clock')
 screen = pg.display.set_mode((WIDTH, HEIGHT))
-
-font = pg.font.SysFont("times New Roman", 35)
 
 clock = pg.time.Clock()
 
@@ -43,10 +37,6 @@ while running:
     angle_min = time(t.minute)
     rotate(screen, h2, (255, 150), angle_sec)
     rotate(screen, h, (255, 150), angle_min)
-    # Нету в задании, дополнительно от Яслана:
-   # text = font.render(f'{t:%H:%M:%S}', True, BLACK)
-    #screen.blit(text, (0,0))
-
     pg.display.flip()
 
 pg.quit()
